31fa959c3771b56d82231b3e16f9cb207d9761cf
CaptainInler <CaptainInler@users.noreply.github.com>
2022-03-27 00:47:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import os
 if __name__== "__main__":
   print("Hello World from the Devops repo!")
   with open('Failed.py', 'w') as file:
     file.write('whatever')
+
+
+  directory_path = os.getcwd()
+  print("My current directory is : " + directory_path)
