ac88a7ad0a91985f153e9ba236608aa94aa459b7
CaptainInler <CaptainInler@users.noreply.github.com>
2022-04-02 15:21:50 +0200

    Update wdComplete.py

--- wdComplete/wdComplete.py
+++ wdComplete/wdComplete.py
@@ -1,7 +1,7 @@
 import os
 if __name__== "__main__":
   print("Hello World from the Devops repo!")
-  with open('Failed.py', 'w') as file:
+  with open('wdCompleteResult', 'w') as file:
     file.write('whatever')
 
 
