a9e7d10a18b3c37e1bfa446f1a6a6e3b13ad4035
CaptainInler <CaptainInler@users.noreply.github.com>
2022-04-03 23:34:18 +0200

    Update wdComplete.py

--- wdComplete.py
+++ wdComplete.py
@@ -1,14 +1,45 @@
 import os
+import sys
 import json
 import requests
+from SPARQLWrapper import SPARQLWrapper
 
 if __name__== "__main__":
   
+  def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+  
+  # Abfrage alle Strassen von OGD
   url = "https://www.ogd.stadt-zuerich.ch/wfs/geoportal/Strassennamenverzeichnis?service=WFS&version=1.1.0&request=GetFeature&outputFormat=GeoJSON&typename=sv_str_verz&propertyname=str_name,str_nr"
   response = json.loads(requests.get(url).text)
   
+  #Abfrage Wikidata
+  endpoint_url = "https://query.wikidata.org/sparql"
+  
   for i in response['features']:
-    print(f"Name: {i['properties']['str_name']}")
+    print(f"Name: {i['properties']['str_name']}, Number: {i['properties']['str_nr']}")
+    name = i['properties']['str_name']
+    query = """SELECT ?streetnameLabel ?x WHERE {
+      ?streetname wdt:P1945 ?streetkey;
+          wdt:P131 wd:Q72;
+                  wdt:P1705 ?x.
+      FILTER regex(?x, '""" + name + """' )
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "de" . } 
+    }"""
+    
+    results = get_results(endpoint_url, query)
+
+    for result in results["results"]["bindings"]:
+        if result:
+          print("ok")
+
+    
+    
   
   with open('wdCompleteResult', 'w') as file:
     file.write('whatever')
